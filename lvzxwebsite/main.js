9cb7a417da388fc3b16d85baea13ba696f71bab8
Xiangggggg <62181129+Xiangggggg@users.noreply.github.com>
2022-01-09 18:50:39 -0500

    finished gallery, basics of slideshow finished

---
+++ lvzxwebsite/main.js
@@ -0,0 +1,58 @@
+/*
+window.onload = () => {
+    const anchors = document.querySelectorAll('a');
+    const transition_el = document.querySelector('.transition');
+  
+    setTimeout(() => {
+      transition_el.classList.remove('is-active');
+    }, 500);
+  
+    for (let i = 0; i < anchors.length; i++) {
+      const anchor = anchors[i];
+  
+      anchor.addEventListener('click', e => {
+        e.preventDefault();
+        let target = e.target.href;
+  
+        console.log(transition_el);
+  
+        transition_el.classList.add('is-active');
+  
+        console.log(transition_el);
+  
+        setInterval(() => {
+          window.location.href = target;
+        }, 500);
+      })
+    }
+  }
+*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
